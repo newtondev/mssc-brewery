36eaef20e598c00061d38fdd71994afc41c5d8a9
Craig Newton <craig@hyvemobile.co.za>
2020-02-13 21:41:25 +0100

    Moved duplicate exception handling code to separate class with ControllerAdvice annotation.

--- src/main/java/com/newtondev/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/com/newtondev/msscbrewery/web/controller/CustomerController.java
@@ -53,15 +53,4 @@ public class CustomerController {
     public void deleteCustomer(@NotNull @PathVariable("customerId") UUID customerId) {
         customerService.deleteById(customerId);
     }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
-        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-
-        e.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
 }
